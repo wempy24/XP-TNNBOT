ce831a2fd462d79e97c4a21745bf9daf288d256d
Xp-Tn <49530313+XP-TN@users.noreply.github.com>
2020-12-08 15:05:59 +0700

    index.js

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ const imageToBase64 = require('image-to-base64');
 const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
+const xp1 = require("./lib/xp1.js");
+const xp2 = require("./lib/xp2.js");
+const xp3 = require("./lib/xp3.js");
+const xp4 = require("./lib/xp4.js");
 const readTextInImage = require('./lib/ocr')
 //
 const
