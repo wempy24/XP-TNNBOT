caab6dfe0cca2193bfa4d6ce5f78f3e2d9b2ad45
Xp-Tn <49530313+XP-TN@users.noreply.github.com>
2020-12-08 16:44:57 +0700

    xp1.js

---
+++ xp1.js
@@ -0,0 +1,28 @@
+exports.xp1 = (id, XP-TN, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) => {
+	return `
+  
+❉──────────❉
+*♦TOOLS1 ${XP-TN}*
+❉──────────❉
+╔════════════════════
+║ *MENU1 BOT ${XP-TN}*
+╠════════════════════
+║╭──❉ *STICKER MAKER & SIMI* ❉──
+║│1. _*.sticker*_
+║│2. _*! <teks>*_
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Group WhatsApp*
+║│ _${groupwa}_
+║│2. *YouTube <subscribe>*
+║│ _${youtube}_
+║│3. *Instagram <Follow>*
+║│ _${instagram}_
+║│4. *Creator ${XP-TN}*
+║│ _${nomer}_
+║╰───────────
+╠════════════════════
+║ _*MADE BY XP-TN*_
+╚════════════════════`
+}
